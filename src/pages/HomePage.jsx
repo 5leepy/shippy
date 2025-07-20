30a5a6d82d91b03651045a415f0efd280a39a986
5leepy <n4deer@gmail.com>
2025-07-20 21:33:55 +0700

    refactor: atur struktur folder dengan pages dan components

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div>
+      <h1>ShippyLabel - Cetak Label Pengiriman</h1>
+      <p>Aplikasi untuk membuat label pengiriman dengan mudah.</p>
+    </div>
+  );
+}
+
+export default HomePage;
